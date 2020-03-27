f88292a93056e839ea761539fe4936e1a10f5d1f
Artur Brasil <arturmbrasil@hotmail.com>
2020-03-27 02:08:44 -0300

    github api get repo

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
@@ -8,14 +9,28 @@ export default class Main extends Component {
   // eslint-disable-next-line react/state-in-constructor
   state = {
     newRepo: '',
+    repositories: [],
   };
 
   handleInputChange = (e) => {
     this.setState({ newRepo: e.target.value });
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+    });
   };
 
   render() {
